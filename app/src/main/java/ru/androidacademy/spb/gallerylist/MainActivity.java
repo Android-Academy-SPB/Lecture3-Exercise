9606696ead40df11818f5b972213ba387df45f1d
Artur Vasilov <avasilov@yandex-team.ru>
2018-04-10 23:49:21 +0300

    Added adapter for cats

--- app/src/main/java/ru/androidacademy/spb/gallerylist/MainActivity.java
+++ app/src/main/java/ru/androidacademy/spb/gallerylist/MainActivity.java
@@ -3,6 +3,8 @@ package ru.androidacademy.spb.gallerylist;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new CatsAdapter(generateCats()));
     }
 
     @NonNull
